643a4e0d3eac9f9c821dbf2c461c8e298b998f8b
Leonardo Theodoro <simplespinheiro@gmail.com>
2026-01-27 16:51:00 +0100

    :lipstick: add @ robin on sidebar

--- apps/web/src/components/app-sidebar.tsx
+++ apps/web/src/components/app-sidebar.tsx
@@ -53,7 +53,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
               <a href="/dashboard">
                 <PineIcon className="!size-5" />
-                <span className="text-base font-semibold">Pine</span>
+                <span className="text-base font-semibold">Pine @ Robin</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
